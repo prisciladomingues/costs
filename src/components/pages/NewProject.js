05d9df08f5d0917f70aca74b79f695a4510f3a74
Priscila <priiscilaa.domingues@gmail.com>
2022-03-19 17:23:31 -0300

    pagina para formulario de projeto

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,6 +1,15 @@
+import ProjectForm from '../project/ProjectForm';
+import styles from './NewProject.module.css'
+
 function NewProject() {
-    return <h1>NewProject</h1>;
-  }
-  
-  export default NewProject;
-  
+  return (
+    <div className={styles.newproject_container}>
+      <h1>Criar Projeto
+        <p>Crie seu projeto para depois adicionar os serviços</p>
+        <ProjectForm />
+      </h1>
+    </div>
+  )
+}
+
+export default NewProject;
